2ff1a82cce76f78016c4b9755a278c88502e4442
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-07 10:44:17 +0200

    Module 003 Delegates und Events

---
+++ HelloDelegates/Program.cs
@@ -0,0 +1,93 @@
+﻿
+
+
+
+namespace HelloDelegates
+{
+    internal class Program
+    {
+        // Function Deklaration fuer einen Function Type den wir spaeter benutzen werden
+        public delegate void Hello(string name);
+
+        static void Main(string[] args)
+        {
+            HelloDelegates();
+
+            Console.WriteLine("\n\nActions und Funcs");
+            ActionSamples();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        #region Delegates
+        private static void HelloDelegates()
+        {
+            var hello = new Hello(HalloDeutschland);
+
+            hello("Max Mustermann"); // Ausfuehrung des Delegates
+            Console.WriteLine();
+
+
+            // Mit += koennen wir weitere function pointers hinzufuegen
+            hello += HalloDeutschland;
+            hello("Alex");
+            Console.WriteLine();
+
+            hello += HelloAmerica;
+            hello += HelloAmerica;
+            hello("Pascale");
+            Console.WriteLine();
+
+            hello -= HelloAmerica;
+            hello -= HelloAmerica;
+            hello!("Tim"); // Mit ! sagen wir dem Compiler, dass hello garantiert nicht null sein kann!
+            Console.WriteLine();
+
+            hello -= HalloDeutschland;
+            hello -= HalloDeutschland;
+            //hello("Tim"); // NullReferenceExeption weil hello null sein wird
+
+            if (hello != null)
+            {
+                hello("mit null check");
+            }
+            hello?.Invoke("Alternative mit ?");
+        }
+
+        private static void HelloAmerica(string name)
+        {
+            Console.WriteLine($"Howdy, my name is {name}!");
+        }
+
+        private static void HalloDeutschland(string name)
+        {
+            Console.WriteLine($"Hallo, mein Name ist {name}!");
+        }
+        #endregion
+
+        private static void ActionSamples()
+        {
+            var printNumber = new Action<int, int>(PrintRandomNumber);
+
+            printNumber(10, 20);
+            // Besser mit null check
+            printNumber?.Invoke(10, 20);
+
+            var addNumber = new Func<int, int, int>((x, y) => x + y);
+            int result = addNumber(10, 20);
+            Console.WriteLine($"10 + 20 = {result}");
+
+            bool IsEven(int number) => number % 2 == 0;
+
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var firstEvenNumber = numbers.ToList().Find(IsEven);
+            Console.WriteLine("First even number is " + firstEvenNumber);
+        }
+
+        private static void PrintRandomNumber(int max, int min)
+        {
+            Console.WriteLine($"Random Number: {new Random().Next(max, min)}");
+        }
+    }
+}
---
+++ HelloEvents/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace HelloEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+            course.CourseStarted += (sender, e) => Console.WriteLine("Kurs gestartet");
+            course.CourseFinished += (sender, e) =>
+            {
+                Console.WriteLine("Kurs beendet");
+                Console.WriteLine(e.contents);
+            };
+
+            Console.WriteLine("Press any key to start the course");
+            Console.ReadKey();
+            course.Start();
+        }
+    }
+
+    public class Course
+    {
+        public record CourseFinishedEventArgs(string contents);
+
+        public event EventHandler CourseStarted;
+
+        public event EventHandler<CourseFinishedEventArgs> CourseFinished;
+
+        public void Start()
+        {
+            // Wenn wir ein Event aufrufen, uebergeben wir den "sender"
+            // welches der this Kontext der Klasse ist (Best Practice)
+            CourseStarted?.Invoke(this, EventArgs.Empty);
+
+            // 2 Sekunden warten
+            Thread.Sleep(2000);
+
+            var args = new CourseFinishedEventArgs("Jetzt ist der Kurs fertig");
+            CourseFinished?.Invoke(this, args);
+        }
+    }
+}
