5825424b0c6c794b723bf86798df0702ccb74e5d
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-07 15:45:54 +0200

    Lab 005 Loesung

---
+++ Lab5SolarSystem/Data/SolarItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lab5SolarSystem.Data
+{
+    [Serializable]
+    public class SolarItem
+    {
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Type")]
+        public SolarItemType Type { get; set; }
+
+        public SolarItem()
+        {
+        }
+
+        public SolarItem(string description, SolarItemType solarItemType)
+        {
+            Description = description;
+            Type = solarItemType;
+        }
+    }
+
+    public enum SolarItemType { Star, Planet, Trabant }
+}
