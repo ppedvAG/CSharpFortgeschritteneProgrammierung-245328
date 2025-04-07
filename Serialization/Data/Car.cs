c52b8452ea1503606c26451ba4d5aefd94624c2d
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-07 15:28:37 +0200

    Module 005 Serialization

---
+++ Serialization/Data/Car.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using System.Drawing;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Serialization.Data
+{
+    public class Car
+    {
+        [XmlIgnore]
+        [JsonIgnore]
+        public long Id { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        [XmlAttribute("VehicleType")]
+        [JsonPropertyName("VehicleType")]
+        public string Type { get; set; }
+
+        public string Fuel { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public KnownColor Color { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Manufacturer: {Manufacturer}, Model: {Model}, Type: {Type}, Fuel: {Fuel}, Color: {Color}";
+        }
+
+        public static List<Car> Generate(int count = 20)
+        {
+            KnownColor[] exclude = Enumerable.Range(1, 27)
+                .Select(i => (KnownColor)i).ToArray();
+
+            return new Faker<Car>()
+                .UseSeed(42)
+                .RuleFor(c => c.Id, f => f.Random.Long())
+                .RuleFor(c => c.Manufacturer, f => f.Vehicle.Manufacturer())
+                .RuleFor(c => c.Model, f => f.Vehicle.Model())
+                .RuleFor(c => c.Type, f => f.Vehicle.Type())
+                .RuleFor(c => c.Fuel, f => f.Vehicle.Fuel())
+                .RuleFor(c => c.Color, f => f.Random.Enum(exclude))
+                .Generate(count);
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,109 @@
+﻿using Serialization.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using System.Xml.Serialization;
+using LegacyJson = Newtonsoft.Json;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = Car.Generate(10);
+
+            Console.WriteLine("Xml Serialization");
+            XmlSerialization(cars);
+
+            Console.WriteLine("\nJson Serialization");
+            JsonSerialization(cars);
+
+            Console.WriteLine("\nJson with Newtonsoft");
+            NewtonsoftSerialization(cars);
+
+            Console.WriteLine("\nJson manuell deserialisieren");
+            ManuellDeserialisieren(cars, "cars.json");
+        }
+
+        private static void XmlSerialization<T>(List<T> cars)
+        {
+            var serializer = new XmlSerializer(typeof(List<T>));
+
+            using (var stream = new FileStream("cars.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, cars);
+            }
+
+            using (var stream = new FileStream("cars.xml", FileMode.Open))
+            {
+                var deserialized = serializer.Deserialize(stream) as List<T>;
+
+                foreach (var car in deserialized)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+
+        private static void JsonSerialization(List<Car> cars)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true, // pretty print
+                ReferenceHandler = ReferenceHandler.IgnoreCycles // rekursionen verhindern
+            };
+            var json = JsonSerializer.Serialize(cars, options);
+            File.WriteAllText("cars.json", json);
+
+            var fileContent = File.ReadAllText("cars.json");
+            var result = JsonSerializer.Deserialize<List<Car>>(fileContent, options);
+
+            foreach (var car in result)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+        private static void NewtonsoftSerialization(List<Car> cars)
+        {
+            var settings = new LegacyJson.JsonSerializerSettings
+            {
+                Formatting = LegacyJson.Formatting.Indented,
+                TypeNameHandling = LegacyJson.TypeNameHandling.Objects, // Vererbung ermoeglichen
+                ReferenceLoopHandling = LegacyJson.ReferenceLoopHandling.Ignore,
+            };
+            var json = LegacyJson.JsonConvert.SerializeObject(cars, settings);
+            File.WriteAllText("cars-newton.json", json);
+
+            var fileContent = File.ReadAllText("cars-newton.json");
+            var result = LegacyJson.JsonConvert.DeserializeObject<List<Car>>(fileContent, settings);
+
+            foreach (var car in result)
+            {
+                Console.WriteLine(car);
+            }
+        }
+        
+        private static void ManuellDeserialisieren(List<Car> cars, string filename)
+        {
+            var json = File.ReadAllText(filename);
+            
+            // JSON-Dokument laden
+            using JsonDocument document = JsonDocument.Parse(json);
+
+            var root = document.RootElement;
+
+            // Daten auslesen
+            foreach (var item in root.EnumerateArray())
+            {
+                string manufacturer = item.GetProperty(nameof(Car.Manufacturer)).GetString();
+                string model = item.GetProperty(nameof(Car.Model)).GetString();
+                string color = item.GetProperty(nameof(Car.Color)).GetString();
+
+                Console.WriteLine($"Manufacturer: {manufacturer}, Model: {model}, Color: {color}");
+            }
+
+        }
+    }
+}
