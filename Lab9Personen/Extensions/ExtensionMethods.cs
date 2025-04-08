628d455848dee39d68fc04a23b969bd78d30b162
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-08 11:07:15 +0200

    Lab 009 Loesung

---
+++ Lab9Personen/Extensions/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using Lab9Personen;
+using System.Text;
+
+namespace Lab9_Personen.Extensions;
+
+public static class ExtensionMethods
+{
+	public static string PrintTop10<T>(this IEnumerable<T> list, Func<T, string> formatter)
+        => list.Take(10).ToString(formatter);
+
+	public static string ToString<T>(this IEnumerable<T> list, Func<T, string> formatter)
+    {
+        return list.Aggregate(new StringBuilder(), AppendLine).ToString();
+
+        StringBuilder AppendLine(StringBuilder agg, T p)
+        {
+            return agg.AppendLine(formatter(p));
+        }
+    }
+
+    public static void ToConsole<T>(this T value, string prefix = "") => Console.WriteLine(prefix + value);
+}
