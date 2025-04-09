03f7f23120f5491c2fcbf221b5c824f341f5c699
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-04-09 08:27:37 +0200

    Lab 012 Loesung

---
+++ TermCalculator/Refactored/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TermCalculator.Refactored
+{
+    public enum CalcOperation { Addition = 1, Subtraction, Multiplication, Division }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var input = GetInput();
+                var term = new Term(input);
+                var result = term.Parse().Calculate();
+
+                Console.WriteLine($"\t={result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fehler: {ex.Message}");
+            }
+        }
+
+        public static string GetInput()
+        {
+            Console.WriteLine("Bitte gib einen Term mit zwei Zahlen und einem Grundrechenoperator (+ - * /) ein (z.B.: 25+13):");
+            return Console.ReadLine() ?? throw new ArgumentNullException("Eingabe darf nicht null sein.");
+        }
+    }
+}
